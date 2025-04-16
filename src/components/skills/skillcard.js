3a41bfc8dc5fd3035f23a97fde6f9f53b6ac86b4
Cobi Hopkins <cobihopkins@yahoo.com>
2025-04-16 08:24:28 +0100

    Update Skill components. Update App.css
    • Update Skill component to render each card with the new AnimationFadeIn component container.
    • Add a new CSS file for skill cards to directly apply classes to the card component.
    • Update App.css to remove redundant code that was previously used for styling.

--- src/components/skills/skillcard.js
+++ src/components/skills/skillcard.js
@@ -1,3 +1,4 @@
+import "./skillscard.css";
 import React from 'react';
 import { Row, Card, Avatar } from 'antd';
 import logo from '../../assets/academic-degree-1.png';
@@ -5,29 +6,28 @@ import logo from '../../assets/academic-degree-1.png';
 const { Meta } = Card;
 
 // todo
-class SkillsCard extends React.Component {
-    render() {
+const SkillsCard = ({ id, title, desc, src = logo }) => {
 
-        return (
-            <Card 
-                style={{width: 300, height: 350, padding: 10, backgroundColor: '#20242D', borderColor: '#20242D', borderRadius: 0, boxShadow: '0 0 3px #13151B', marginTop: 15, marginRight: 15, marginLeft: 15}}
-                hoverable={false}
-                >
-                    <Row type='flex' justify={'center'}>
-                        <Avatar size={75} src={this.props.src} shape='square' style={{marginTop: 20, marginBottom: 40}} alt="An SVG image of a blue and white graduation cap encased in a white circle." />
-                    </Row>
-                    <Meta title={
-                        <>
-                            <p style={{color: 'white', fontSize: 17, fontWeight: 700, padding: 0, margin: 0}}>{this.props.title}</p>
-                        </>
-                    } description={
-                        <>
-                            <p style={{color: 'lightgray'}}>{this.props.desc}</p>
-                        </>
-                    } />
-                </Card>
-        )
-    }
+    return (
+        <Card 
+            className="skill-card"
+            hoverable={false} key={id}
+            >
+                <Row type='flex' justify={'center'}>
+                    <Avatar size={75} src={src} shape='square' style={{marginTop: 20, marginBottom: 40}} alt="An SVG image of a blue and white graduation cap encased in a white circle." />
+                </Row>
+                <Meta title={
+                    <>
+                        <p style={{color: 'white', fontSize: 17, fontWeight: 700, padding: 0, margin: 0}}>{title}</p>
+                    </>
+                } description={
+                    <>
+                        <p style={{color: 'lightgray'}}>{desc}</p>
+                    </>
+                } />
+            </Card>
+    )
+
 }
 
 SkillsCard.defaultProps = {
--- src/components/skills/skillsgrid.js
+++ src/components/skills/skillsgrid.js
@@ -1,22 +1,16 @@
 import React, {useRef, useEffect} from 'react';
 import { Row, Col } from 'antd';
 import SkillsCard from './skillcard';
+import AnimationFadeIn from '../animationFadeIn/animationFadeIn';
 
 const skillData = [
     {id: 1, title: 'Git Version Control', desc: 'Proficient usage of Git for version control, including branching, merging, and resolving conflicts. Experienced with collaboration using GitHub.'},
     {id: 2, title: 'JavaScript', desc: 'Skilled in JavaScript for creating dynamic and interactive web applications. Experience with ES6+, frameworks like React, NextJS, and asynchronous programming.'},
-    {id: 3, title: 'Backend Development', desc: 'Backend Development using NodeJS (Koa, Express, Next) and Python Flask.'},
+    {id: 3, title: 'Backend Development', desc: 'Backend Development using NodeJS (Koa, Express, Next), Python Flask, and SQL.'},
     {id: 4, title: 'Python (ML/AI)', desc: 'Project experience using Python for backend web development and Machine Learning / AI.'},
     {id: 5, title: 'C# (.NET MAUI)', desc: 'Mobile Development using C# and .NET MAUI. Implementation using MVVM and Supabase.'},
     {id: 6, title: 'CSS3', desc: 'Project experience using CSS3 to develop a responsive UI.'}
 ];
-
-const applyAnimation = (entries) => {
-    const [entry] = entries;
-    if (entry.isIntersecting) {
-        entry.target.classList.add('skill-animation');
-    }
-}
 
 function SkillsGrid() {
 
@@ -26,35 +20,25 @@ function SkillsGrid() {
     *  3. Check element compliance with WCAG Guidelines.
     */
 
-    const containerRef = useRef(null);
-
-    useEffect(() => {
-        const observer = new IntersectionObserver(applyAnimation, {threshold: 0.20});
-        if (containerRef.current) observer.observe(containerRef.current);
-
-        return () => {
-            if (containerRef.current) observer.unobserve(containerRef.current);
-            observer.disconnect();
-        }
-    }, []);
-
-        const skillsList = skillData.map(skill => {
-            return (
-                <div style={{padding: '10px'}} key={'skill_' + skill.id}>
-                    <Col span={8}>
-                        <SkillsCard {...skill} />
-                    </Col>
-                </div>
-            )
-        });
-
         return (
             <>
                 <Row type='flex' justify='center' style={{width: '100%'}}>
                     <h1 style={{fontSize: '40px'}}>Skills</h1>
                 </Row>
-                <Row className="skills-list" ref={containerRef}>
-                    {skillsList}
+                <Row style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', pb: 60, flexWrap: 1, maxWidth: '1050px', alignItems: 'stretch', paddingBottom: 20 }}>
+                    {skillData.map((skill, index) => {
+                        
+                        return (
+                                <AnimationFadeIn delay={index * 0.25}>
+                                    <Col style={{ minHeight: 380 }}>
+                                    <SkillsCard
+                                        id={skill.id}
+                                        title={skill.title}
+                                        desc={skill.desc} />
+                                    </Col>
+                                </AnimationFadeIn>
+                        )
+                    })}
                 </Row>
             </>
         );
