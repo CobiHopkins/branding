471f90af1384450d751db9dd38faaa49e6c1ae36
Cobi Hopkins <cobihopkins@yahoo.com>
2025-04-29 10:53:17 +0100

    Fix CSS-related issues for Nav & Hero.
    • Fix an issue where navigation would wrap due to no font-size changes.
    • Fix an issue with the Hero SVG which would cause visual issues on mobile.
    • Added 'overflow: hidden' to Hero SVG to combat the issue.
    • SVG No longer visible on mobile due to CSS changes.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,7 +17,6 @@ function Hero() {
 
     return (
         <Row type='flex' justify='space-around' id="hero" style={{height: 'calc(100vh - 64px)', backgroundColor: '#20242D', color: 'white'}}>
-            <svg className="bg-wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#31333b" fillOpacity="1" d="M0,288L48,288C96,288,192,288,288,288C384,288,480,288,576,261.3C672,235,768,181,864,181.3C960,181,1056,235,1152,229.3C1248,224,1344,160,1392,128L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
             <Row className="hero-container" style={{ alignSelf: 'center'}}>
                     <Row type='flex' className="hero-image" justify='center'>
                             <Avatar id="heroImg" size={{
@@ -40,6 +39,9 @@ function Hero() {
                         <SocialLinks links={socialMediaLinks}/>
                     </Row>
                     </Col>
+                    <Row>
+                        <svg className="bg-wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none"><path fill="#31333b" fillOpacity="1" d="M0,288L48,288C96,288,192,288,288,288C384,288,480,288,576,261.3C672,235,768,181,864,181.3C960,181,1056,235,1152,229.3C1248,224,1344,160,1392,128L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+                    </Row>
                 </Row>
             </Row>
     )
