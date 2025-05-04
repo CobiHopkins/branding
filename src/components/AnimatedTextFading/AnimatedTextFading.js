fe39092385c9c0241132a7bd7b3cae78c8a79ca2
Cobi Hopkins <cobihopkins@yahoo.com>
2025-05-04 20:57:18 +0100

    Remove unused MUI Box component from AnimatedTextFading component

--- src/components/AnimatedTextFading/AnimatedTextFading.js
+++ src/components/AnimatedTextFading/AnimatedTextFading.js
@@ -1,5 +1,5 @@
 import { useState, useEffect} from "react";
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import "./AnimatedTextFading.css";
 /**
  * 
