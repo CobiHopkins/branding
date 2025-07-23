e9c336e04325722e3fd4c63a56b74b035da2d877
Cobi Hopkins <cobihopkins@yahoo.com>
2025-07-23 17:55:15 +0100

    Updating package and manifest files. Updated AnimatedLoadingBalls to support themes.

--- src/components/AnimatedLoadingBalls/AnimatedLoadingBalls.js
+++ src/components/AnimatedLoadingBalls/AnimatedLoadingBalls.js
@@ -1,13 +1,22 @@
 import "./AnimatedLoadingBalls.css";
+import { useTheme } from '@mui/material/styles';
 import { Stack, Box } from '@mui/material';
 
 const AnimatedLoadingBalls = ({ pxSize = 30 }) => {
 
+    const theme = useTheme();
+
+    const styles = {
+        "--background-color": theme.palette.secondary.dark,
+        "--ball-width": `${pxSize}px`,
+        "--ball-height": `${pxSize}px`
+    }
+
     return (
         <Stack aria-label="Loading content" direction='row' alignItems='center' justifyItems='center' justifyContent='center' spacing={1} sx={{ height: "150px"}}>
-            <Box component="div" className="loading-ball ball-one" sx={{ width: `${pxSize}px`, height: `${pxSize}px`}}></Box>
-            <Box component="div" className="loading-ball ball-two" sx={{ width: `${pxSize}px`, height: `${pxSize}px`}}></Box>
-            <Box component="div" className="loading-ball ball-three" sx={{ width: `${pxSize}px`, height: `${pxSize}px`}}></Box>
+            <Box component="div" className="loading-ball ball-one" sx={styles}></Box>
+            <Box component="div" className="loading-ball ball-two" sx={styles}></Box>
+            <Box component="div" className="loading-ball ball-three" sx={styles}></Box>
         </Stack>
     )
 }
